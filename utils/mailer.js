ddfb121b312a537da672a3bfe168d8edc1c6d935
Huzaifah <mhuzaifah101@gmail.com>
2025-08-29 17:53:12 +0100

    resend

--- utils/mailer.js
+++ utils/mailer.js
@@ -1,31 +1,27 @@
-const nodemailer = require('nodemailer');
+const { Resend } = require('resend');
 require('dotenv').config();
 
-const transporter = nodemailer.createTransport({
-  service: 'SendGrid',
-  auth: {
-    user: 'apikey',
-    pass: process.env.SENDGRID_API_KEY,
-  },
-});
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendResetEmail = async (to, token) => {
-    const resetUrl = `supplicate://reset-password?token=${token}`;
+  const resetUrl = `supplicate://reset-password?token=${token}`;
 
-    await transporter.sendMail({
-        from: `"Supplicate" <${process.env.EMAIL_USER}>`,
-        to,
-        subject: 'Password Reset Request',
-        html: `
-            <h1>Password Reset Request</h1>
-            <p>Click the link below to reset your password:</p>
-            <a href="${resetUrl}">Reset Password</a>
-            <p>If you did not request this, please ignore this email.</p>
-        `,
-    })
-
-}
-
-module.exports = {
-    sendResetEmail,
+  try {
+    const { data } = await resend.emails.send({
+      from: process.env.EMAIL_FROM,
+      to,
+      subject: 'Password Reset Request',
+      html: `
+        <h1>Password Reset Request</h1>
+        <p>Click the link below to reset your password:</p>
+        <a href="${resetUrl}">Reset Password</a>
+        <p>If you did not request this, please ignore this email.</p>
+      `,
+    });
+    console.log('Reset email sent successfully!', data);
+  } catch (err) {
+    console.error('Error sending reset email:', err);
+  }
 };
+
+module.exports = { sendResetEmail };
